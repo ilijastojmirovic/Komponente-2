25491b39b938b65e1461e9a57619fadae9e4a601
ilijastojmirovic <115079288+ilijastojmirovic@users.noreply.github.com>
2023-12-16 20:34:51 +0100

    Add files via upload

---
+++ src/main/java/com/example/userserivsapp/UserServisApp/UserServisAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.userserivsapp.UserServisApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServisAppApplication.class, args);
+	}
+
+}
